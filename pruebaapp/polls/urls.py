7132f0f92a7f4cffc3d01ab5a9ca785a4ea79179
EdwardBarrero <edwbarrero@gmail.com>
2023-01-22 18:42:53 -0500

    second commit

--- pruebaapp/polls/urls.py
+++ pruebaapp/polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-  path("", views.index, name="index")
+  path("", views.index, name="index"),
+  path("<int:question_id>", views.detail, name="detail"),
+  path("result/<int:question_id>", views.results, name="results"),
+  path("votes/<int:question_id>", views.votes, name="votes")
 ]
--- pruebaapp/polls/views.py
+++ pruebaapp/polls/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+
+from .models import Question
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hola mundo")
+  lastest_questions_list = Question.objects.all()
+  return render(request, "polls/index.html", {
+    "lastest_questions_list": lastest_questions_list
+  })
+
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  print(question)
+  return render(request, "polls/detail.html", {
+    "question": question
+  })
+
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return HttpResponse(question)
+
+def votes(request, question_id):
+  return HttpResponse(f"Estas votando a la pregunta número {question_id}")
